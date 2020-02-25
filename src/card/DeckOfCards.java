bd3884f08bff3cff9eeb2f5c07d6250a843e2f7b
Patrick Craig <pbull2112@gmail.com>
2020-02-25 21:39:09 +0000

    added functionality to shuffle and draw from card deck

--- src/card/DeckOfCards.java
+++ src/card/DeckOfCards.java
@@ -1,6 +1,7 @@
 package card;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import card.enums.Rank;
@@ -10,7 +11,7 @@ public class DeckOfCards {
 	private Suit[] permittedSuits;
 	private Rank[] permittedRanks;
 	private List<Card> cards;
-	
+
 	public DeckOfCards(Suit[] permittedSuits, Rank[] permittedRanks) {
 		this.permittedSuits = permittedSuits;
 		this.permittedRanks = permittedRanks;
@@ -29,6 +30,10 @@ public class DeckOfCards {
 		return cards;
 	}
 
+	public final int getNumberOfCards() {
+		return cards.size();
+	}
+
 	private final List<Card> createCards() {
 		final List<Card> cards = new ArrayList<Card>();
 
@@ -38,7 +43,19 @@ public class DeckOfCards {
 				cards.add(card);
 			}
 		}
-		
+
 		return cards;
+	}
+
+	public final void shuffle() {
+		Collections.shuffle(this.cards);
+	}
+
+	// return card drawn from deck, remove it from the deck. 0-based index
+	public final Card draw(final int cardNumber) throws IndexOutOfBoundsException {
+		final Card drawnCard = this.cards.get(cardNumber);
+		this.cards.remove(cardNumber);
+
+		return drawnCard;
 	}
 }
--- src/main.java
+++ src/main.java
@@ -10,10 +10,30 @@ public class main {
 	public static void main(String[] args) {
 		DeckOfCards deckOfCards = new StandardDeckOfCards();
 
+		deckOfCards.shuffle();
+
 		for (Card card : deckOfCards.getCards()) {
-			System.out.println("Suit = " + card.getSuit() + " Rank = "
-					+ card.getRank());
+			printCard(card);
 		}
+
+		printDeckSize(deckOfCards);
+
+		System.out.println("drawing card...");
+		Card card = deckOfCards.draw(0);
+
+		printCard(card);
+
+		printDeckSize(deckOfCards);
+	}
+
+	private static void printDeckSize(final DeckOfCards deckOfCards) {
+		System.out.println("number of cards = "
+				+ deckOfCards.getNumberOfCards());
+	}
+
+	private static void printCard(final Card card) {
+		System.out.println("Suit = " + card.getSuit() + " Rank = "
+				+ card.getRank());
 	}
 
 }
