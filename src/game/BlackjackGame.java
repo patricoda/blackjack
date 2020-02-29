3089bc66ae590841caf6c6576bee36f6ca22beff
Patrick Craig <pbull2112@gmail.com>
2020-02-29 21:03:13 +0000

    add player actions to blackjack game loop

--- src/game/BlackjackGame.java
+++ src/game/BlackjackGame.java
@@ -2,7 +2,9 @@ package game;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import card.Card;
 import card.deck.StandardDeckOfCards;
 import card.hand.Hand;
 
@@ -27,6 +29,7 @@ public class BlackjackGame {
 		dealCards();
 
 		printParticipantValues();
+		initGameLoop();
 	}
 
 	private void addPlayers() {
@@ -48,5 +51,61 @@ public class BlackjackGame {
 			System.out.println(player.getName() + " has "
 					+ player.getHand().getHandValue());
 		}
+	}
+
+	private void initGameLoop() {
+		//TODO need to create a standard input output class
+		Scanner scanner = new Scanner(System.in);
+this.players.add(new Player("Karl"));
+this.players.get(1).setHand(new Hand(this.dealer.deal(2)));
+		for (Player player : this.players) {
+			System.out.println("\n" + player.getName() + ":");
+			boolean playerAction = true;
+			
+			do {
+				System.out.println("current total = " + player.getHand().getHandValue());
+				
+				if(player.getHand().getHandValue() == 21) {
+					playerAction = false;
+				}
+				
+				System.out.println("hit (1) or stand (2)?");
+				
+				if (scanner.hasNext("1") || scanner.hasNext("2")) {
+					int action = scanner.nextInt();
+					switch (action) {
+					case 1:
+						Card newCard = this.dealer.deal(1).get(0);
+						System.out.println("dealer deals " + player.getName() + " a new card");
+						System.out.println("new card is " + newCard.getRank() + " of " + newCard.getSuit() + "s");
+						player.getHand().addCard(newCard);
+						break;
+					case 2:
+						System.out.println("stand");
+						playerAction = false;
+						break;
+					}
+					
+					if(player.getHand().getHandValue() > 21) {
+						playerAction = false;
+					}
+				} else {
+					System.out.println("invalid operation");
+					scanner.next();
+				}
+			} while (playerAction);
+			
+			System.out.println(player.getName() + "'s current total is " + player.getHand().getHandValue());
+			
+			if(player.getHand().getHandValue() > 21) {
+				System.out.println("BUST!");
+			} else if(player.getHand().getHandValue() == 21) {
+				System.out.println("BLACKJACK!");
+			}
+		}
+
+		scanner.close();
+		// dealer will attempt to get best hand
+		// round finish
 	}
 }
